7fefc8d75ba9d17c7a9fb7308ad2973eefe46b73
Li Cai <licai@lis-macbook-pro.lan>
2021-12-13 00:02:19 -0500

    add synthesize function

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import redirect, render
 from .models import Document
 from .forms import DocumentForm
+from .synthesize import synthesize
+import os
 
 
 def my_view(request):
     print(f"Great! You're using Python 3.6+. If you fail here, use the right version.")
-    message = 'Upload as many files as you want!'
+    message = 'You can upload multiple videos!'
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            newdoc = Document(docfile=request.FILES['docfile'])
-            newdoc.save()
+            doc = Document(docfile=request.FILES['docfile'])
+            doc.save()
+
+            base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            dir = os.path.join(base, 'media', doc.docfile.name),
+            synthesize(dir[0])
 
             # Redirect to the document list after POST
             return redirect('my-view')
